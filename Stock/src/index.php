75c9472b8736f49040fdbd01eedfdf0858f8bde8
Viktor Muratov <muratovv@ukr.net>
2023-06-27 14:29:13 +0300

    Update index.php

--- Stock/src/index.php
+++ Stock/src/index.php
@@ -9,32 +9,51 @@ spl_autoload_register(function (string $className) {
 });
 
 $dao = new Dao();
+$dao->createDatabase();
+$dao->createUserTable();
 $view = new View('C:\xampp\htdocs\Stock\templates');
 $controllerLogin = new LoginController($dao, $view);
 $controllerStock = new StockController($dao, $view);
-session_start();
 
 if (isset($_POST['addAction']))
 {
+    session_start();
     $controllerStock->addItem($_SESSION['user_id'], $_POST['name'], $_POST['typenumber'], $_POST['number'], $_POST['price'], $_POST['allprice'], $_POST['resultprice']);
 } 
 else if(isset($_POST['removeAction'])) 
 {
+    session_start();
     $controllerStock->removeItem($_SESSION['user_id'], $_POST['id']);
 }
 else if(isset($_POST['editAction'])) 
 {
+    session_start();
     $controllerStock->updateItem($_SESSION['user_id'], $_POST['id'], $_POST['name'], $_POST['typenumber'], $_POST['number'], $_POST['price'], $_POST['allprice'], $_POST['resultprice']);
 }
 else if(isset($_POST['removeAllAction'])) 
 {
+    session_start();
     $controllerStock->removeAllItems($_SESSION['user_id']);
 }
 else if(isset($_POST['loginAction'])) 
 {
     $userId = $controllerLogin->login($_POST['login'], $_POST['password']);
+    if ($userId != -1) 
+    {
+        $status = session_status();
+    if($status == PHP_SESSION_NONE){
+        session_start();
+    }else
+    if($status == PHP_SESSION_ACTIVE){
+        session_destroy();
+        session_start();
+    }
     $_SESSION['user_id'] = $userId;
-    ($userId != -1) ? $controllerStock->main($userId) : $controllerLogin->main(); 
+    $controllerStock->main($userId);
+    } else 
+    {
+        $controllerLogin->main(); 
+    }
 }
 else if(isset($_POST['registerFormAction'])) 
 {
